431b80fb371c869e0b0f7860e11c01ecd1285e8c
Miroslav Stoyanov <miro.stoyanov23@gmail.com>
2025-06-06 15:59:57 +0300

    Update IPHashLoadBalancer tests for new hash

--- src/loadBalancers/tests/IPHashLoadBalancer.test.ts
+++ src/loadBalancers/tests/IPHashLoadBalancer.test.ts
@@ -12,20 +12,20 @@ describe('IPHashLoadBalancer', () => {
     });
 
     it('should get the next active server in a round-robin fashion', () => {
-        // The IP hash of 'client1' corresponds to 'server1'
-        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server1');
-        // The IP hash of 'client2' corresponds to 'server2'
-        expect(loadBalancer.getServerForRequest('client2')?.url).toBe('server2');
-        // The IP hash of 'client3' corresponds to 'server3'
-        expect(loadBalancer.getServerForRequest('client3')?.url).toBe('server3');
-        // The IP hash of 'client4' corresponds to 'server1'
-        expect(loadBalancer.getServerForRequest('client4')?.url).toBe('server1');
+        // The IP hash of 'client1' corresponds to 'server2'
+        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server2');
+        // The IP hash of 'client2' corresponds to 'server3'
+        expect(loadBalancer.getServerForRequest('client2')?.url).toBe('server3');
+        // The IP hash of 'client3' corresponds to 'server1'
+        expect(loadBalancer.getServerForRequest('client3')?.url).toBe('server1');
+        // The IP hash of 'client4' corresponds to 'server2'
+        expect(loadBalancer.getServerForRequest('client4')?.url).toBe('server2');
     });
 
     it('should add a new server', () => {
         loadBalancer.addServer('server4');
-        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server1');
-        expect(loadBalancer.getServerForRequest('client5')?.url).toBe('server4');
+        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server3');
+        expect(loadBalancer.getServerForRequest('client5')?.url).toBe('server3');
     });
 
     it('should remove a server', () => {
@@ -36,18 +36,18 @@ describe('IPHashLoadBalancer', () => {
 
     it('should disable a server', () => {
         loadBalancer.disableServer('server2');
-        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server1');
+        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server2');
         expect(loadBalancer.getServerForRequest('client2')?.url).toBe('server3');
     });
 
     it('should enable a server', () => {
         loadBalancer.disableServer('server2');
-        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server1');
+        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server2');
         expect(loadBalancer.getServerForRequest('client2')?.url).toBe('server3');
 
         loadBalancer.enableServer('server2');
-        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server1');
-        expect(loadBalancer.getServerForRequest('client2')?.url).toBe('server2');
+        expect(loadBalancer.getServerForRequest('client1')?.url).toBe('server2');
+        expect(loadBalancer.getServerForRequest('client2')?.url).toBe('server3');
     });
 
     it('should handle empty server list', () => {
