0e977c85f5df9d705cd6bea4cc1ae89292803f5d
Miroslav Stoyanov <miro.stoyanov23@gmail.com>
2025-08-10 23:48:46 +0300

    Revert "chore: scaffold analytics platform monorepo"

--- analytics-platform/apps/event-processor/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { EventProcessorModule } from './event-processor.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(EventProcessorModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
